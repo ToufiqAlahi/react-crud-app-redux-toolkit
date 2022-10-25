999d883fc340f0a395e743c11af7faf8efc2da6a
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-10-25 22:23:59 +0600

    Create Home.js

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div>
+        This is home!
+        <h1 className="text-7xl ">Hello </h1>
+        <button className="btn my-5 btn-primary">Button</button>
+      </div>
+    );
+};
+
+export default Home;
